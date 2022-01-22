1599f2188bb55f0632342bd8bc93c8ac572c8273
IanHsieh <energy0702@gmail.com>
2022-01-22 10:04:23 +0800

    add comment

--- stock-be/server.js
+++ stock-be/server.js
@@ -1,9 +1,11 @@
+// 引入 express
 const express = require("express");
 
 require("dotenv").config();
 
+// 利用 express 這個 library 來建立一個web app (express instance)
 let app = express();
 const port = process.env.SERVER_PORT || 3000;
 app.listen(port, () => {
-  console.log(`簡易版 server 已經啟動 在port ${port} 上`);
+  console.log(`Server running at port ${port}`);
 });
